ce5b860296a1285f936df49955f14c6008124ba4
AROOJ64 <areejmaher64@gmail.com>
2024-09-07 22:05:25 +0300

    Traps

---
+++ Assets/Areej/Scripts/EnemyProjectile.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class EnemyProjectile : MonoBehaviour
+{
+    [SerializeField] private float speed = 1.5f;
+    [SerializeField] private float resetTime = 5.0f;
+
+    private float arrowLifeTime;
+
+    public void ActivateProjectile()
+    {
+        arrowLifeTime = 0;
+        gameObject.SetActive(true);
+    }
+    private void Update()
+    {
+        float movementSpeed = speed * Time.deltaTime;
+        transform.Translate(Vector3.right * movementSpeed);
+        arrowLifeTime += Time.deltaTime;
+        if (arrowLifeTime > resetTime)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        gameObject.SetActive(false);
+    }
+
+}
