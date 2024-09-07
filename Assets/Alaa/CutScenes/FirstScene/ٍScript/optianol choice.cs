2373cf1a57e78b10d16a6186f325a1d2661528ca
Alaa FouaD <141962934+AlaaFouad123@users.noreply.github.com>
2024-09-07 14:38:54 +0300

    first scene updata

---
+++ Assets/Alaa/CutScenes/FirstScene/ٍScript/optianol choice.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class optianolchoice : MonoBehaviour
+{
+
+    public GameObject Alert;
+    public GameObject YesButtonUI;
+    public GameObject NoButtonUI;
+    public GameObject Alert2;
+
+    public void Start()
+    {
+        Alert.SetActive(true);
+        Alert2.SetActive(false);
+        NoButtonUI.SetActive(false);
+        YesButtonUI.SetActive(false);
+    }
+    public void YesButton()
+    {
+        Alert.SetActive(false);
+        NoButtonUI.SetActive(false);
+        YesButtonUI.SetActive(true);
+        Alert2.SetActive(false);
+    }
+
+
+    public void NoButton()
+    {
+        Alert.SetActive(false);
+        NoButtonUI.SetActive(true);
+        YesButtonUI.SetActive(false);
+        Alert2.SetActive(false);
+    }
+
+    public void YesYes()
+    {
+        SceneManager.LoadScene("Start");
+    }
+
+    public void NoNo()
+    {
+        Alert2.SetActive(true);
+        NoButtonUI.SetActive(false);
+    }
+}
