4a8ec5c9ac0806bdf6f2f642502e7e2d418f7d98
Alaa FouaD <141962934+AlaaFouad123@users.noreply.github.com>
2024-09-11 18:43:56 +0300

    pause Update

--- Assets/Alaa/pause.cs
+++ Assets/Alaa/pause.cs
@@ -3,22 +3,42 @@ using UnityEngine;
 public class Pause : MonoBehaviour
 {
     public GameObject menu;
+    private bool isPaused = false; 
 
-    void Start()
+    public GameObject dialog;
+
+    public void Start()
     {
-      
-        menu.SetActive(false);
+        Time.timeScale = 1;
+    }
+    void Update()
+    {
+        
+        // Check for Esc key press
+        if (Input.GetKeyDown(KeyCode.Escape) && !dialog.activeInHierarchy)
+        {
+            if (isPaused)
+            {
+                ResumeGame(); // If currently paused, resume the game
+            }
+            else
+            {
+                PauseGame(); // If not paused, pause the game
+            }
+        }
     }
 
     public void PauseGame()
     {
         Time.timeScale = 0f;
-        menu.SetActive(true); 
+        menu.SetActive(true);
+        isPaused = true; // Update pause state
     }
 
     public void ResumeGame()
     {
-        Time.timeScale = 1f; 
+        Time.timeScale = 1f;
         menu.SetActive(false);
+        isPaused = false; // Update pause state
     }
 }
